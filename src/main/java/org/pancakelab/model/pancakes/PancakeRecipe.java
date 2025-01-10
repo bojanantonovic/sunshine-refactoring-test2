06d3b74862ffc0791da1a657d294c3165664a934
Bojan Antonovic <bojan@antonovic.ch>
2025-01-10 21:17:15 +0100

    PancakeRecipe is now a Record

--- src/main/java/org/pancakelab/model/pancakes/PancakeRecipe.java
+++ src/main/java/org/pancakelab/model/pancakes/PancakeRecipe.java
@@ -2,16 +2,7 @@ package org.pancakelab.model.pancakes;
 
 import java.util.List;
 
-public class PancakeRecipe {
-	private final List<String> ingredients;
-
-	public PancakeRecipe(List<String> ingredients) {
-		this.ingredients = ingredients;
-	}
-
-	public List<String> ingredients() {
-		return ingredients;
-	}
+public record PancakeRecipe(List<String> ingredients) {
 
 	public String description() {
 		return "Delicious pancake with %s!".formatted(String.join(", ", ingredients()));
